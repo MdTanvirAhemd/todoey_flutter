6ec5dab57eaab88c77423182950258c230d5b7ae
Overlord <tanvir1005@student.sust.edu>
2022-01-26 23:06:31 +0600

    update

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:todoey_flutter/widgets/tasks_tile.dart';
+import 'package:todoey_flutter/models/task.dart';
 
-class TasksList extends StatelessWidget {
+
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListStae createState() => _TasksListState();
+
+  class _TasksListState extends State<TasksList>{
+    List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget> [
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
+        TaskTile(isChecked: true,taskTitle: "How are you", checkboxCallback: (cen){},longPressCallback: (row){}),
+        //TaskTile(),
+       // TaskTile(),
       ],
     );
   }
